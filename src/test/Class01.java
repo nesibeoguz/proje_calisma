116aa80825252df897e0d82f2476ee27c77085d0
NesibeOguz <n.asutay@hotmail.com>
2023-06-16 17:12:40 +0300

    değişiklik yaptım.

--- src/test/Class01.java
+++ src/test/Class01.java
@@ -3,5 +3,8 @@ package test;
 public class Class01 {
     public static void main(String[] args) {
         System.out.println("nesibe branşı açtım");
+        System.out.println("nesibe branşı açtım");
+        System.out.println("nesibe branşı açtım");
+        System.out.println("nesibe branşı açtım");
     }
 }
