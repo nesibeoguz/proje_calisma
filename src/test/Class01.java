afbe289e16168d8b790f17e17af7c090a741cb79
NesibeOguz <n.asutay@hotmail.com>
2023-06-16 17:00:35 +0300

    değişiklik yaptım.

--- src/test/Class01.java
+++ src/test/Class01.java
@@ -4,5 +4,6 @@ public class Class01 {
     public static void main(String[] args) {
         System.out.println("nesibe branşı açtım");
         System.out.println("nesibe yazdı");
+        System.out.println("nesibe yazdı2");
     }
 }
