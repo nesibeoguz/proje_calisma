b6b38ff3aeeda0992fb27c7ceee4c5f4685f1661
NesibeOguz <n.asutay@hotmail.com>
2023-06-16 17:37:52 +0300

    değişiklik yaptım.

--- src/test/Class01.java
+++ src/test/Class01.java
@@ -3,12 +3,11 @@ package test;
 public class Class01 {
     public static void main(String[] args) {
 
-<<<<<<<<< Temporary merge branch 1
-=========
+
         System.out.println("aysegul");
 
         //Değiştirdim
         System.out.println("ben geldim");
->>>>>>>>> Temporary merge branch 2
+
     }
 }
